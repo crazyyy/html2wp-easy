54ffd0aa2b221043378858341f37aa4146ec147a
Vitaly A <crazyyy@gmail.com>
2015-01-08 22:04:33 +0200

    rework packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 'use strict';
 // generated on 2014-09-06 using generator-gulp-bootstrap 0.0.4
 
-var gulp = require('gulp');
+var     gulp    = require('gulp');
+// load plugins
+var     $       = require('gulp-load-plugins')();
+
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var gutil = require('gulp-util');
 
-// load plugins
-var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function () {
     return gulp.src('app/css/main.scss')
